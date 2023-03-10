7967de089f9b53f215af871a914de76a71dedf25
vagon3452 <vagon3452@gmail.com>
2023-03-11 02:16:54 +0500

    1

--- src/components/Modal/ModalInfo/Modal.js
+++ src/components/Modal/ModalInfo/Modal.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useCallback, useEffect } from "react";
 import styles from "./Modal.module.css";
 import ReactDOM from "react-dom";
 import { CloseIcon } from "@ya.praktikum/react-developer-burger-ui-components";
@@ -6,38 +6,43 @@ import { CloseIcon } from "@ya.praktikum/react-developer-burger-ui-components";
 const modalRoot = document.getElementById("react-modals");
 
 function Modal({ data, closeModal }) {
-  console.log(data);
-  
-  function close(e) {
-    e.stopPropagation()
-    closeModal(false);
-  }
+  const close = useCallback(
+    (e) => {
+      (e.keyCode === 27 || e.type === "click") && closeModal(false);
+    },
+    [closeModal]
+  );
+
+  useEffect(() => {
+    document.addEventListener("keydown", close);
+
+    return () => {
+      document.removeEventListener("keydown", close);
+
+    };
+  }, [close]);
 
   return ReactDOM.createPortal(
     <div className={styles.modalBackground} onClick={close}>
-      <div className={styles.content} onClick={e=>e.stopPropagation()}>
+      <div className={styles.content} onClick={(e) => e.stopPropagation()}>
         <div className={styles.title}>
           <p className="text text_type_main-large">Детали ингридиента</p>
           <div>
             <CloseIcon type="primary" onClick={close} />
           </div>
         </div>
-        <img src={data.image}  className={styles.image} />
+        <img src={data.image} alt={data.name} className={styles.image} />
         <div className={styles.frame}>
           <p className="text text_type_main-medium">{data.name}</p>
         </div>
         <div className={styles.nutrition}>
           <div className={styles.value}>
             <p className="text text_type_main-default">Калории,ккал</p>
-            <p className="text text_type_digits-default" >
-              {data.calories}
-            </p>
+            <p className="text text_type_digits-default">{data.calories}</p>
           </div>
           <div className={styles.value}>
             <p className="text text_type_main-default">Белки, г </p>
-            <p className="text text_type_digits-default">
-              {data.proteins}
-            </p>
+            <p className="text text_type_digits-default">{data.proteins}</p>
           </div>
           <div className={styles.value}>
             <p className="text text_type_main-default">Жиры, г</p>
